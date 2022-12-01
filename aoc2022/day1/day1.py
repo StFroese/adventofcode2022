b2e3da0cbcfabcd0f9cc953cae2d3808fd80f61a
Stefan Fröse <stefan.froese@udo.edu>
2022-12-01 07:41:35 +0100

    faster part 2

--- aoc2022/day1/day1.py
+++ aoc2022/day1/day1.py
@@ -14,13 +14,19 @@ def main():
     print(max_calories)
 
     # Part 2
+    calories_reduce = calories.copy()
     top_three = []
     for i in range(3):
-        max_calories = max(calories)
+        max_calories = max(calories_reduce)
         top_three.append(max_calories)
-        calories.remove(max_calories)
+        calories_reduce.remove(max_calories)
 
     total_to_three = sum(top_three)
+    print(total_to_three)
+
+    # Part 2 alternative
+    calories.sort(reverse=True)
+    total_to_three = sum(calories[:3])
     print(total_to_three)
 
 if __name__ == '__main__':
