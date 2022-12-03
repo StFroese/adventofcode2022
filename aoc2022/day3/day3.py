6f5410274b72a26189f15dc61b7861c53e423961
Stefan Fröse <stefan.froese@udo.edu>
2022-12-03 11:44:32 +0100

    day3

---
+++ aoc2022/day3/day3.py
@@ -0,0 +1,23 @@
+def read_input():
+    with open('input.txt', 'r') as f:
+        lines = [[set(list(line[:len(line)//2])),set(list(line[len(line)//2:]))] for line in f.read().split('\n')[:-1]]
+    return lines
+
+def priority(char):
+    if char.islower():
+        return ord(char) - ord('a') + 1
+    else:
+        return ord(char) - ord('A') + 27
+
+def main():
+    input = read_input()
+
+    # part 1
+    print(sum([priority(rucksack[0].intersection(rucksack[1]).pop()) for rucksack in input]))
+
+    # part 2 (it was a mistake to convert the input to sets directly...)
+    print(sum([priority(input[3*i][0].union(input[3*i][1]).intersection(input[3*i+1][0].union(input[3*i+1][1])).intersection(input[3*i+2][0].union(input[3*i+2][1])).pop()) for i in range(len(input)//3)]))
+
+
+if __name__ == '__main__':
+    main()
