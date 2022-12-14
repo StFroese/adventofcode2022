1c024d522ec3e9d8c45013ae4db47b9edbb5ba05
Stefan Fröse <stefan.froese@udo.edu>
2022-12-14 14:47:29 +0100

    plot

--- aoc2022/day14/day14.py
+++ aoc2022/day14/day14.py
@@ -1,4 +1,7 @@
 from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
 
 def read_input(test=False):
     file = 'test_input.txt' if test else 'input.txt'
@@ -48,6 +51,53 @@ def cave_depth(cave):
 def count_sand(cave):
     return sum([val == 'o' for val in cave.values()])
 
+def plot_cave(cave):
+    max_x = max([key[0] for key in cave.keys()])+1
+    max_y = max([key[1] for key in cave.keys()])+1
+
+    grid = np.zeros((max_x, max_y))
+    for key in cave.keys():
+        if cave[key] == '#':
+            grid[key] = 1
+        else:
+            grid[key] = 2
+
+    min_x = min([key[0] for key in cave.keys()])+1
+
+    plt.imshow(grid[min_x:,:].T, cmap='inferno')
+    plt.savefig('cave.pdf', dpi=600)
+
+def gif(cave, steps, min_x, max_x, max_y):
+    fig = plt.figure()
+    depth = cave_depth(cave)
+    sand = {'x': 500, 'y': 0}
+
+    grid = np.zeros((max_x, max_y))
+    for key in cave.keys():
+        if cave[key] == '#':
+            grid[key] = 1
+        else:
+            grid[key] = 2
+
+    im = plt.imshow(grid[min_x:,:].T, cmap='inferno', interpolation=None, vmin=0, vmax=3)
+
+    def animate(i):
+        move_sand(sand, cave, depth)
+        grid = np.zeros((max_x, max_y))
+        for key in cave.keys():
+            if cave[key] == '#':
+                grid[key] = 1
+            else:
+                grid[key] = 2
+
+        grid[sand['x'], sand['y']] = 3
+        im.set_data(grid[min_x:,:].T)
+        return [im]
+
+
+    ani = FuncAnimation(fig, animate, interval=40, blit=True, frames=steps, repeat=True)
+    ani.save("cave.gif", dpi=300,  writer=PillowWriter(fps=50))
+
 def main():
     cave = read_input(test=False)
 
@@ -68,6 +118,15 @@ def main():
     while move_sand(sand, cave, depth):
         pass
     print(count_sand(cave))
+
+    plot_cave(cave)
+
+    steps = count_sand(cave)
+    min_x = min([key[0] for key in cave.keys()])+1
+    max_x = max([key[0] for key in cave.keys()])+1
+    max_y = max([key[1] for key in cave.keys()])+1
+    cave = read_input(test=False)
+    gif(cave, steps, min_x, max_x, max_y)
 
 
 if __name__ == '__main__':
