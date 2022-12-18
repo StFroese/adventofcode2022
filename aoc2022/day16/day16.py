d3bca06157df74c9202257aaa48b5a5ea112691f
Stefan Fröse <stefan.froese@udo.edu>
2022-12-18 13:18:40 +0100

    day 16 part 1

---
+++ aoc2022/day16/day16.py
@@ -0,0 +1,114 @@
+from tqdm import tqdm
+
+def read_input(test=False):
+    file = "test_input.txt" if test else "input.txt"
+    g = Graph()
+    with open(file, "r") as f:
+        for line in f.read().split('\n')[:-1]:
+            words = line.split(' ')
+            g.add_node(words[1], int(words[4][5:-1]))
+            to_nodes = list(map(lambda x: x[:-1] if x[-1]==',' else x,words[9:]))
+            for to_node in to_nodes:
+                g.add_edge(words[1], to_node)
+    return g
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+        self.rates = {}
+
+    def add_node(self, node, rate):
+        self.nodes.append(node)
+        self.rates[node] = rate
+
+    def add_edge(self, from_node, to_node):
+        if from_node in self.edges:
+            self.edges[from_node].append(to_node)
+        else:
+            self.edges[from_node] = [to_node]
+
+
+
+
+def bfs(graph, start, rates):
+    q = []
+    distances = {}
+    explored = [start]
+    q.append(start)
+    distances[start] = 0
+    while q:
+        node = q.pop(0)
+        for edge in graph.edges[node]:
+            if edge not in explored:
+                explored.append(edge)
+                q.append(edge)
+                distances[edge] = distances[node] + 1
+    return distances
+
+def paths_in_time(distances, time, rates):
+    # this is DFS???
+    pressures = []
+    paths = []
+    q = []
+    minutes = []
+    pressure = []
+    q.append(['AA'])
+    minutes.append(time)
+    pressure.append(0)
+    while q:
+        path = q.pop()
+        node = path[-1]
+        minute = minutes.pop()
+        pres = pressure.pop()
+        end = False
+        for edge in distances[node]:
+            dist = distances[node][edge]
+            # + 1 to open valve
+            if dist + 1 > minute or edge in path or rates[edge] == 0: # don't look at zero flow valves
+                end = True
+                continue
+            end = False
+            minutes.append(minute - dist - 1) # -1 to open valve
+            pressure.append(pres + minutes[-1] * rates[edge])
+            q.append(path + [edge])
+        if end:
+            pressures.append(pres)
+    return pressures
+
+
+def main():
+    graph = read_input(test=False)
+
+    # part 1
+    # let's think about this again
+    # we can decide to find every path in 30 minutes and choose the one with the maximum pressure / DFS
+    # -> this will take long because of all combinations
+
+    # better approach:
+    # calculate the minute distance from each node to each node
+    # calculate all pressures in that can be obtained in time
+    # -> build a complete graph with distances/minutes
+
+    # first calculate distance from each to each -> use BFS for this??
+    distances = {}
+    for from_node in graph.nodes:
+        distances[from_node] = {}
+        distance_nodes = bfs(graph, from_node, graph.rates)
+        for to_node in distance_nodes:
+            if to_node != from_node:
+                distances[from_node][to_node] = distance_nodes[to_node]
+    print(distances)
+    # we have the distances from each node to each node now. nice. what next??
+    # calculate all paths that can be reached in time
+    # works on test but is slow in input -> remove valves thats have zero flow!
+    pressure = paths_in_time(distances, 30, graph.rates)
+    print(max(pressure))
+
+    # part 2
+
+
+
+
+if __name__ == "__main__":
+    main()
